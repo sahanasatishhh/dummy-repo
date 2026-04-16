9b59a4aacf51cc7134cf5fe6e3d5026fe05f04de
Sahana Satish <sahanasatish22@gmail.com>
2026-04-15 21:06:49 -0700

    unicode encoded secret test

---
+++ unicode_test.py
@@ -0,0 +1,3 @@
+# Base64 encoded secret that contains unicode when decoded
+# "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ123456" encoded with a unicode char mixed in
+ENCODED_SECRET = "Z2hwX2FCY0RlRmdIaUprTG1Ob1BxUnNUdVZ3WHlaMTIzNDU2\u00e9"
